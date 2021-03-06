2deed2a8489336665b9bf90a7c51af2eac475b6f
backenddev <74377541+imbackenddev@users.noreply.github.com>
2021-03-06 16:45:43 -0500

    Add files via upload

--- uniqlo_cookies.js
+++ uniqlo_cookies.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
-const mongo_db_url = 'mongodb+srv://bach:bach30121234@devices.psqu0.mongodb.net/Akamai-API?retryWrites=true&w=majority';
+const mongo_db_url = 'mongodb+srv://bach:bach30121234@devices.psqu0.mongodb.net/Akamai-Devices?retryWrites=true&w=majority';
 mongoose.connect(mongo_db_url, { useUnifiedTopology: true, useNewUrlParser : true }).then(() => {
-	console.log('Uniqlo');
+	console.log('Devices');
 }).catch((err) => {
 	console.log(err);
 });
 
-const UniqloSchema = new Schema({
-	store : {
-		type : String,
+const deviceSchema = new Schema({
+	site : {
+		type : String, 
 		required : true
 	},
 	abck_cookie : {
@@ -18,6 +18,6 @@ const UniqloSchema = new Schema({
 	}
 });
 
-const Uniqlo = mongoose.model('uniqlo', UniqloSchema); 
+const Device = mongoose.model('uniqlo', deviceSchema); 
 
 module.exports = Uniqlo; 
