75eb4907c06b22c9dde387e0443d725a7a615be4
backenddev <74377541+imbackenddev@users.noreply.github.com>
2021-03-06 17:59:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/cookie_api', function(req, res){
 
 app.get('/uniqlo/cookie_api', function(req, res){
 	//res.sendFile(path.join(__dirname + '/collector.html'));
-	Uniqlo.findOne({}, (error,data) => {
+	Uniqlo.findOneAndDelete({}, (error,data) => {
 		if(error){
 			res.send('no cookies');
 		}else{
